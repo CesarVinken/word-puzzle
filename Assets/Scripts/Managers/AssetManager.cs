8d7e8e81ddb45939dc26df28647250cda7a19e2c
CesarVinken <39000090+CesarVinken@users.noreply.github.com>
2023-05-19 10:21:44 +0300

    Implemented CharacterTileFactory using addressables 2

--- Assets/Scripts/Managers/AssetManager.cs
+++ Assets/Scripts/Managers/AssetManager.cs
@@ -1,23 +1,65 @@
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
-public class AssetManager : MonoBehaviour
+public class CharacterTileFactory
 {
-    public static AssetManager Instance;
+    private static AssetReferenceGameObject _characterTilePrefabReference;
 
-    [SerializeField] private GameObject _characterTilePrefab;
-
-    public void Awake()
+    public static void Create(Transform parentTransform, CharacterTileHandler characterTileHandler, CharacterTileDataModel characterTileData)
     {
-        Instance = this;
+        AssetReferenceGameObject prefabReference = GetPrefabReference();
 
-        if (_characterTilePrefab == null)
+        AsyncOperationHandle<GameObject> handle = Addressables.InstantiateAsync(prefabReference, parentTransform);
+
+        handle.Completed += (o) =>
         {
-            ConsoleLog.Error(LogCategory.General, $"Could not find character tile prefab");
-        }
+            if (o.Status == AsyncOperationStatus.Succeeded)
+            {
+                OnAssetLoaded(handle, characterTileHandler, characterTileData);
+            }
+            else
+            {
+                Debug.LogError($"Failed to instantiate tile active event");
+            }
+        };
     }
 
-    public GameObject GetCharacterTilePrefab()
+    private static AssetReferenceGameObject GetPrefabReference()
     {
-        return _characterTilePrefab;
+        if (_characterTilePrefabReference != null)
+        {
+            return _characterTilePrefabReference;
+        }
+
+        AssetReferenceGameObject prefabReference = new AssetReferenceGameObject($"CharacterTile.prefab");
+
+        if (prefabReference == null)
+        {
+            ConsoleLog.Error(LogCategory.General, $"Could not find an asset for _characterTilePrefabReference");
+        }
+
+        if (_characterTilePrefabReference != null)
+        {
+            return _characterTilePrefabReference;
+        }
+
+        _characterTilePrefabReference = prefabReference;
+
+        return prefabReference;
+    }
+
+    private static void OnAssetLoaded(AsyncOperationHandle<GameObject> handle, CharacterTileHandler characterTileHandler, CharacterTileDataModel characterTileData)
+    {
+        RectTransform tileRectTransform = handle.Result.GetComponent<RectTransform>();
+
+        Vector3 tilePosition = new Vector3((characterTileData.TilePosition.x * 12), characterTileData.TilePosition.y * 12, characterTileData.TilePosition.z);
+
+        tileRectTransform.localPosition = tilePosition;
+
+        CharacterTileMono characterTile = handle.Result.GetComponent<CharacterTileMono>();
+
+        characterTile.Setup(characterTileData);
+        characterTileHandler.OnCharacterTileCreated(handle.Result, characterTile);
     }
 }
